19f8660e7bc6890ec4d10a868b5ebd31c46c6ebf
Joshua Meyer <joshua.meyer1@yahoo.ca>
2022-01-04 20:55:52 -0500

    made rpi_boost_gauge

---
+++ rpi_boost_gauge.py
@@ -0,0 +1,19 @@
+import obd
+from obd import OBDCommand, Unit
+from obd.protocols import ECU
+from obd.utils import bytes_to_int
+
+connection = obd.OBD()
+
+def boost(messages):
+    """decoder for boost messages"""
+    d = messages[0].data
+    d = d[1:]
+    v = bytes_to_int(d) / 3.0
+    return v * Unit.Turbocharger 
+
+boost_pressure = OBDCommand("Turbocharger compressor inlet pressure",
+"Turbo pressure", "016F", 3, boost, ECU.ALL, True)
+
+connection.supported_commands.add(boost_pressure)
+connection.query(boost_pressure)
