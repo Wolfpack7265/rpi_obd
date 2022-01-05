702bc63b876aa38c3cd6ac641f05815eb93ca489
Joshua Meyer <joshua.meyer1@yahoo.ca>
2022-01-05 00:55:48 -0500

    boost pressure works

--- rpi_boost_gauge.py
+++ rpi_boost_gauge.py
@@ -2,7 +2,7 @@ import obd
 from obd import OBDCommand, Unit
 from obd.protocols import ECU
 from obd.utils import bytes_to_int
-
+loop = True
 connection = obd.OBD(portstr="COM4", baudrate="38400", protocol=None, fast=True, timeout=0.1, check_voltage=True, start_low_power=False) 
 
 def boost(messages):
@@ -10,11 +10,12 @@ def boost(messages):
     d = messages[0].data
     d = d[1:]
     v = bytes_to_int(d) / 3.0
-    return v * Unit.Turbocharger 
+    return v 
 
 boost_pressure = OBDCommand("Turbocharger compressor inlet pressure",
 "Turbo pressure", b"016F", 3, boost, ECU.ALL, True)
 
 connection.supported_commands.add(boost_pressure)
-response = connection.query(boost_pressure)
-print(response.value)
+while True:
+    response = connection.query(boost_pressure)
+    print(response.value)
