d45d65532916587d90400c3d3c5d23f97fdcd79e
Joshua Meyer <joshua.meyer1@yahoo.ca>
2022-01-15 19:57:50 -0500

    added color to inner gauge border

--- rpi_boost_gauge.py
+++ rpi_boost_gauge.py
@@ -1,4 +1,4 @@
-from tkinter.constants import ALL
+from tkinter.constants import ALL, NONE
 from tkinter.font import BOLD
 import obd
 import time
@@ -9,9 +9,11 @@ import tkinter as tk
 from tkinter import Label, filedialog, Text 
 import tkinter.ttk
 import os 
+
 loop = False
 gauge_sweep_bool = False
 connection = obd.OBD(portstr="COM4", baudrate="38400", protocol=None, fast=True, timeout=0.1, check_voltage=True, start_low_power=False) 
+#connection = obd.OBD()
 intake = obd.commands.INTAKE_PRESSURE
 barometric = obd.commands.BAROMETRIC_PRESSURE
 oil_temp = obd.commands.OIL_TEMP
@@ -27,6 +29,14 @@ nominal_color = "DarkOrange1"
 red_zone_color = "red"
 gauge_sweep_1 = 220
 gauge_sweep_2 = -40
+
+grey_zone_arc = (grey_zone - min_boost)/(max_boost - min_boost)
+red_zone_arc = (red_zone - min_boost)/(max_boost - min_boost)
+arc_length_1 = (grey_zone_arc*(max_gauge - min_gauge))+ min_gauge
+arc_length_1 = round(arc_length_1, 2)
+arc_length_2 = ((red_zone_arc*(max_gauge - min_gauge))+ min_gauge) 
+arc_length_2 = round(arc_length_2, 2)
+
 root = tk.Tk()
 canvas = tk.Canvas(root, width=480, height=480, borderwidth=0, highlightthickness=0,
 bg="black")
@@ -65,33 +75,48 @@ while loop == False:
     elif gauge_sweep_1 > -40:
         gauge_sweep_1 = (gauge_sweep_1 -5)
         canvas.create_circle(240, 240, 230, fill="black", outline= gauge_color, width=4 )
-        canvas.create_circle_arc(240, 240, 180, style="arc", outline= gauge_color, width=4, start=220, end=-40)
+        #canvas.create_circle_arc(240, 240, 180, style="arc", outline= gauge_color, width=4, start=220, end=-40)
+        canvas.create_circle_arc(240, 240, 180, style="arc", outline= grey_zone_color, width=4, start= 220, end= arc_length_1)
+        canvas.create_circle_arc(240, 240, 180, style="arc", outline= nominal_color, width=4, start= arc_length_1, end= arc_length_2)
+        canvas.create_circle_arc(240, 240, 180, style="arc", outline= red_zone_color, width=4, start= arc_length_2, end=-40)
         canvas.create_circle_arc(240, 240, 195, style="arc", outline= gauge_color, width=70, start=220, end=230)
         canvas.create_circle_arc(240, 240, 195, style="arc", outline= gauge_color, width=70, start=310, end=320)
-        lead_arc = canvas.create_circle_arc(240, 240, 205, style="arc", outline="white", width=60, start=gauge_sweep_1 , end= gauge_sweep_1-1)
-        boost_arc_1 = canvas.create_circle_arc(240, 240, 205, style="arc", outline="grey", width=50, start=220, end= gauge_sweep_1)
+        if gauge_sweep_1 ==0:
+            lead_arc = canvas.create_circle_arc(240, 240, 205, style="arc", outline="white", width=60, start=2 , end= 2 -1)
+        else:
+            lead_arc = canvas.create_circle_arc(240, 240, 205, style="arc", outline="white", width=60, start=gauge_sweep_1 , end= gauge_sweep_1-1)
+        
         canvas.update()
         canvas.update_idletasks()
         canvas.delete(lead_arc)
-        canvas.delete(boost_arc_1)
+        
 
     elif gauge_sweep_1 == -40 and gauge_sweep_2 >= -40 and gauge_sweep_2 <=215:
         gauge_sweep_2 = (gauge_sweep_2 +5)
         canvas.create_circle(240, 240, 230, fill="black", outline= gauge_color, width=4 )
         canvas.create_circle_arc(240, 240, 180, style="arc", outline= gauge_color, width=4, start=220, end=-40)
+        canvas.create_circle_arc(240, 240, 180, style="arc", outline= grey_zone_color, width=4, start= 220, end= arc_length_1)
+        canvas.create_circle_arc(240, 240, 180, style="arc", outline= nominal_color, width=4, start= arc_length_1, end= arc_length_2)
+        canvas.create_circle_arc(240, 240, 180, style="arc", outline= red_zone_color, width=4, start= arc_length_2, end=-40)
         canvas.create_circle_arc(240, 240, 195, style="arc", outline= gauge_color, width=70, start=220, end=230)
         canvas.create_circle_arc(240, 240, 195, style="arc", outline= gauge_color, width=70, start=310, end=320)
-        lead_arc = canvas.create_circle_arc(240, 240, 205, style="arc", outline="white", width=60, start=gauge_sweep_2 , end= gauge_sweep_2)
-        boost_arc_1 = canvas.create_circle_arc(240, 240, 205, style="arc", outline="grey", width=50, start=220, end= gauge_sweep_2)
+        if gauge_sweep_2 ==0:
+            lead_arc = canvas.create_circle_arc(240, 240, 205, style="arc", outline="white", width=60, start=2 , end= 2 -1)
+        else:
+            lead_arc = canvas.create_circle_arc(240, 240, 205, style="arc", outline="white", width=60, start=gauge_sweep_2 , end= gauge_sweep_2-1)
+       
         canvas.update()
         canvas.update_idletasks()
         canvas.delete(lead_arc)
-        canvas.delete(boost_arc_1)
+        
         
     elif gauge_sweep_1 == -40 and gauge_sweep_2 == 220:
        
         canvas.create_circle(240, 240, 230, fill="black", outline= gauge_color, width=4 )
         canvas.create_circle_arc(240, 240, 180, style="arc", outline= gauge_color, width=4, start=220, end=-40)
+        canvas.create_circle_arc(240, 240, 180, style="arc", outline= grey_zone_color, width=4, start= 220, end= arc_length_1)
+        canvas.create_circle_arc(240, 240, 180, style="arc", outline= nominal_color, width=4, start= arc_length_1, end= arc_length_2)
+        canvas.create_circle_arc(240, 240, 180, style="arc", outline= red_zone_color, width=4, start= arc_length_2, end=-40)
         canvas.create_circle_arc(240, 240, 195, style="arc", outline= gauge_color, width=70, start=220, end=230)
         canvas.create_circle_arc(240, 240, 195, style="arc", outline= gauge_color, width=70, start=310, end=320)
         canvas.update()
@@ -106,21 +131,19 @@ while loop ==True:
     
     intake_response = connection.query(intake)
     barometric_response = connection.query(barometric)
-    boost = ((intake_response.value.magnitude - barometric_response.value.magnitude)*0.145038)  #units of kilopascals to psi
+    boost = ((intake_response.value.magnitude - barometric_response.value.magnitude)*0.145038) + 10   #units of kilopascals to psi
     boost = round(boost, 2) # float is truncated to 2 decimals with round()
     temp = (boost - min_boost)/(max_boost - min_boost)
-    grey_zone_arc = (grey_zone - min_boost)/(max_boost - min_boost)
-    red_zone_arc = (red_zone - min_boost)/(max_boost - min_boost)
-    arc_length_1 = (grey_zone_arc*(max_gauge - min_gauge))+ min_gauge
-    arc_length_1 = round(arc_length_1, 3)
-    arc_length_2 = ((red_zone_arc*(max_gauge - min_gauge))+ min_gauge) 
-    arc_length_2 = round(arc_length_2, 3)
+    
     arc_length_3 = ((temp*(max_gauge - min_gauge))+ min_gauge)
-    arc_length_3 = round(arc_length_3, 3)
+    arc_length_3 = round(arc_length_3, 2)
     
     
     canvas.create_circle(240, 240, 230, fill="black", outline= gauge_color, width=4 )
     canvas.create_circle_arc(240, 240, 180, style="arc", outline= gauge_color, width=4, start=220, end=-40)
+    canvas.create_circle_arc(240, 240, 180, style="arc", outline= grey_zone_color, width=4, start= 220, end= arc_length_1)
+    canvas.create_circle_arc(240, 240, 180, style="arc", outline= nominal_color, width=4, start= arc_length_1, end= arc_length_2)
+    canvas.create_circle_arc(240, 240, 180, style="arc", outline= red_zone_color, width=4, start= arc_length_2, end=-40)
     canvas.create_circle_arc(240, 240, 195, style="arc", outline= gauge_color, width=70, start=220, end=230)
     canvas.create_circle_arc(240, 240, 195, style="arc", outline= gauge_color, width=70, start=310, end=320)
     boost_arc_1 = canvas.create_circle_arc(240, 240, 205, style="arc", outline="grey", width=50, start=220, end=220)
