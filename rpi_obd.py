f0f6b40ff6f4e6f1e34bc5313086d7ae820fba79
Joshua Meyer <joshua.meyer1@yahoo.ca>
2023-05-22 01:46:43 -0400

    fixed gear indicator not deleting, adding test

--- rpi_obd.py
+++ rpi_obd.py
@@ -11,6 +11,7 @@ import tkinter.ttk
 import os 
 import sys
 import math
+import random
 from bluetooth import * 
 
 #python3 -m elm -s car
@@ -83,6 +84,7 @@ mk7_golf_automatic = [0.63246, 4.46, 2.51, 1.56, 1.14, 0.85, 0.67]
 jk_jeep_wrangler = [1, 4.46, 2.61, 1.72, 1.25, 1, 0.767]
 test_car = [1, 1, 2, 3, 4, 5, 6, 7, 8]
 calculated_gear_ratio = 0
+previous_gear_ratio = 0
 first_gear = None
 second_gear = None
 third_gear = None
@@ -93,7 +95,7 @@ seventh_gear = None
 eigth_gear = None
 ninth_gear = None
 tenth_gear = None
-tolerance = 0.02
+tolerance = 0.03
 
 connection = obd.Async(portstr="/dev/rfcomm0", baudrate=None, protocol=None, fast=True, timeout=0.1, check_voltage=True, start_low_power=False)
 #connection = obd.Async(portstr="COM4", baudrate="38400", protocol=None, fast=True, timeout=0.1, check_voltage=True, start_low_power=False) 
@@ -492,13 +494,16 @@ while loop ==True:
         canvas.tag_lower(boost_arc_3)
         canvas.tag_lower(boost_arc_4)
     
-    if rpm > 1000 & speed != 0:
+    if rpm > 2000:
+        #calculated_gear_ratio = vehicle[random.randint(1, 6)]
         calculated_gear_ratio = ((rpm)*(tire_size)*0.06)/(speed)
         compute_gear_ratio(calculated_gear_ratio)
         if current_gear != 0:
-           gear_text = canvas.create_text(240, 240, text=current_gear, fill= "white", font=("Helvetica", 150, 'bold'))  
+           gear_text = canvas.create_text(240, 240, text=current_gear, fill= "white", font=("Helvetica", 120, 'bold'))  
+           
     else:
         calculated_gear_ratio = 0
+        gear_text = canvas.create_text(240, 240, text="", fill= "black", font=("Helvetica", 1, 'bold'))  
 
     canvas.update()
     canvas.update_idletasks()
@@ -510,7 +515,6 @@ while loop ==True:
     canvas.delete(fuel_bar)
     canvas.delete(fuel_bar_arc)
     canvas.delete(fuel_text)
-    if current_gear != 0:
-        canvas.delete(gear_text)
+    canvas.delete(gear_text)
     #canvas.delete(intake_value)
     #canvas.delete(coolant_value)
